f0b6ac56390606c0d8f1fa6daf4918975b143c33
Secrist <secristn@leidos.com>
2023-12-01 15:27:23 -0600

    completed part 2 of day 1

--- 2023/day1/day1.go
+++ 2023/day1/day1.go
@@ -5,8 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 	"unicode"
 )
+
+func GetDigitWords() []string {
+	return []string{
+		"zero",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+}
 
 func main() {
 	input, err := ReadLines("input.txt")
@@ -16,35 +35,118 @@ func main() {
 
 	calibrationValue := 0
 	for _, line := range input {
-		fd, ld := FindFirstAndLastDigits(line)
+		fd := FindFirstOrLastDigit(line, false, true)
+		ld := FindFirstOrLastDigit(line, true, true)
 		calibrationValue += fd*10 + ld
 	}
 
 	fmt.Println("Calibration Value: ", calibrationValue)
 }
 
-func FindFirstAndLastDigits(s string) (fD, lD int) {
-	firstIndex := -1
-	lastIndex := -1
+// Reverses a string
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
 
-	// Iterate through the string to find the first and last integers
-	for i, char := range s {
-		// Check if the character is a digit
-		if unicode.IsDigit(char) {
-			// If it's the first integer, set firstIndex
-			if firstIndex == -1 {
-				firstIndex, lastIndex = i, i
+	return string(runes)
+}
+
+func FindFirstOrLastDigit(s string, lastDigit bool, includeWords bool) (digit int) {
+
+	if lastDigit {
+		s = ReverseString(s)
+	}
+
+	r := regexp.MustCompile(`(\d)?([a-z]+)?(\d)?`)
+
+	match := r.FindStringSubmatch(s)
+
+	if match[1] != "" {
+		if unicode.IsDigit(rune(match[1][0])) {
+			digit = int(match[1][0] - '0')
+			return
+		} else {
+			digit = TryConvertToDigit(match[1], lastDigit)
+
+			if digit > -1 {
+				return
 			}
+		}
+	} else if match[2] != "" {
+		if unicode.IsDigit(rune(match[2][0])) {
+			digit = int(match[2][0] - '0')
+			return
+		} else {
+			digit = TryConvertToDigit(match[2], lastDigit)
 
-			// Update lastIndex with each occurrence
-			lastIndex = i
+			if digit > -1 {
+				return
+			}
 		}
 	}
 
-	fD = int(s[firstIndex] - '0')
-	lD = int(s[lastIndex] - '0')
-
+	digit, _ = strconv.Atoi(string(match[3][0]))
 	return
+}
+
+func TryConvertToDigit(s string, isReversed bool) (result int) {
+	resultMap := make(map[int]int)
+
+	if !isReversed {
+		for _, word := range GetDigitWords() {
+			if strings.Contains(s, word) {
+				resultMap[strings.Index(s, word)] = GetIntFromWord(word)
+			}
+		}
+	} else {
+		for _, word := range GetDigitWords() {
+			if strings.Contains(s, ReverseString(word)) {
+				resultMap[strings.Index(s, ReverseString(word))] = GetIntFromWord(ReverseString(word))
+			}
+		}
+	}
+
+	if len(resultMap) < 1 {
+		return -1
+	}
+
+	keys := make([]int, 0, len(resultMap))
+	for k := range resultMap {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	return resultMap[keys[0]]
+}
+
+func GetIntFromWord(w string) int {
+	switch w {
+	case "one", "eno":
+		return 1
+	case "two", "owt":
+		return 2
+	case "three", "eerht":
+		return 3
+	case "four", "ruof":
+		return 4
+	case "five", "evif":
+		return 5
+	case "six", "xis":
+		return 6
+	case "seven", "neves":
+		return 7
+	case "eight", "thgie":
+		return 8
+	case "nine", "enin":
+		return 9
+	case "zero", "orez":
+		return 0
+	default:
+		return -1
+	}
 }
 
 // Read Lines from file with input path
